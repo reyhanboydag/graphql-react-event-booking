f9a6c61286414adcf96859d95fd31e59d171f146
reyhanboydag <reyhanboydag@gmail.com>
2022-11-21 14:16:15 +0300

    database-models

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/event");
 
 const app = express();
 
@@ -43,17 +46,41 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc }; //return { ...event._doc, _id: event._doc._id.toString() };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: args.eventInput.title,
+        //   description: args.eventInput.description,
+        //   price: +args.eventInput.price,
+        //   date: args.eventInput.date,
+        // };
+        // events.push(event);
+
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
+          date: new Date(args.eventInput.date),
+        });
+
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch((err) => console.log(err));
+
         return event;
       },
     },
@@ -61,4 +88,12 @@ app.use(
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gitwf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log("here");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
