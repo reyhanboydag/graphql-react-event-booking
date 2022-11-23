f46d586dad2c0c4fe3f7d5a2e99ef805854f8e22
reyhanboydag <reyhanboydag@gmail.com>
2022-11-23 13:25:00 +0300

    added login

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../../models/user");
 
@@ -20,6 +21,24 @@ module.exports = {
       const result = await user.save();
 
       return { ...result._doc };
+    } catch (error) {
+      throw error;
+    }
+  },
+  login: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        throw new Error("User does not exits!");
+      }
+      const isEqual = await bcrypt.compare(password, user.password);
+      if (!isEqual) {
+        throw new Error("Invalid password!");
+      }
+      const token = jwt.sign({ userId: user.id, email: user.email }, "somesupersecretkey", {
+        expiresIn: "1h",
+      });
+      return { userId: user._id, token, tokenExpiration: 1 };
     } catch (error) {
       throw error;
     }
